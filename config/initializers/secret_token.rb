7283ed741bb6cb5d1a867c2edc26c4ee32d7e9e5
bsaral <betul.saral@bil.omu.edu.tr>
2012-10-01 23:32:45 +0300

    muafiyet:start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muafiyet::Application.config.secret_token = 'fe16ff3d947a8d10c11e5575d4969f592c64fcc0b5564bd862e8e81124694532def78def0948b2e9063841050515b7b13c36e2ce883def43d5c587d3aae0732d'
