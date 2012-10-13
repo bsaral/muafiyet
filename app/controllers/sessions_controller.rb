7293cae1cc59a3db67649870be746640422320ce
bsaral <betul.saral@bil.omu.edu.tr>
2012-10-14 01:19:53 +0300

    .

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
         session[:user_id] = user.id
               case user.role
-              when "guest"
+              when "student"
                 redirect_to ("/deneme")
               when "admin"
                 redirect_to ("/deneme")
